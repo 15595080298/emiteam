f81f6043f7f496b2669ff832aad22a03d3e17633
shzhshes <shzhshes@163.com>
2016-04-03 10:21:58 +0800

    ceshi

---
+++ yinfanwan/src/main/java/project/cheng/com/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package project.cheng.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
